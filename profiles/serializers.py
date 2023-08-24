6d50aa45706a634f5c20461b26884922c06e5f4a
mojtababht <m.beheshti831@gmail.com>
2023-08-24 16:37:52 +0330

    custom serializer for current user

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,7 @@ from djoser.serializers import UserSerializer as BaseUserSerializer
 class UserSerializer(BaseUserSerializer):
     is_staff=serializers.BooleanField(read_only=True)
     class Meta(BaseUserSerializer.Meta):
+        ref_name = 'ProfilesUser'
         fields = ['id', 'phone_no', 'first_name', 'last_name', 'is_staff']
 
 
